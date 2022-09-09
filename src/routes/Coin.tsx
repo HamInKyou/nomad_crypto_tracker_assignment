3c2fadf49006a44e40c4d91ecd09844c0c607805
HamInGyu <hfire1207@gmail.com>
2022-09-10 03:40:41 +0900

    상세페이지 overview부분 완료

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,8 +1,183 @@
-import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
+import { fetchCoinInfo, fetchCoinTickers } from "../api";
+
+const Title = styled.h1`
+  font-size: 48px;
+  font-weight: 700;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const BackButton = styled.button`
+  font-size: 48px;
+  border: 0;
+  background-color: inherit;
+  cursor: pointer;
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid;
+  border-color: ${(props) => props.theme.textColor};
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-weight: 600;
+  span:first-child {
+    font-size: 10px;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface LocationState {
+  state: {
+    name: string;
+  };
+}
+
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
 
 function Coin() {
   const { coinId } = useParams();
-  return <h1>Coin : {coinId}</h1>;
+  const navigate = useNavigate();
+  const { state } = useLocation() as LocationState;
+  const { isLoading: infoLoading, data: infoData } = useQuery<IInfoData>(
+    ["info", coinId],
+    () => fetchCoinInfo(`${coinId}`)
+  );
+  const { isLoading: tickersLoading, data: tickersData } = useQuery<IPriceData>(
+    ["tickers", coinId],
+    () => fetchCoinTickers(`${coinId}`),
+    {
+      refetchInterval: 5000,
+    }
+  );
+  const loading = infoLoading || tickersLoading;
+  const onClickBack = () => {
+    navigate(-1);
+  };
+  return (
+    <Container>
+      <Header>
+        <Title>
+          <BackButton onClick={onClickBack}>⬅️</BackButton>
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+        </Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>Rank:</span>
+              <span>{infoData?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Symbol:</span>
+              <span>${infoData?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Price:</span>
+              <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{infoData?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{tickersData?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{tickersData?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+        </>
+      )}
+    </Container>
+  );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -41,7 +41,7 @@ const Coin = styled.li`
 
 const Title = styled.h1`
   font-size: 48px;
-  font-weight: 500;
+  font-weight: 600;
   color: ${(props) => props.theme.accentColor};
 `;
 
