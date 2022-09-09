1c2cbdb5cd4706f908c730b35be1bfd6d210ca7f
HamInGyu <hfire1207@gmail.com>
2022-09-10 03:05:17 +0900

    테마 세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import GlobalStyle from "./GlobalStyle";
+import { lightTheme, darkTheme } from "./theme";
 
+const Header = styled.header`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+const ModeToggleBtn = styled.button`
+  background-color: inherit;
+  border: 0;
+  font-size: 30px;
+  cursor: pointer;
+`;
 function App() {
-  return <div className="App"></div>;
+  const [isDark, setIsDark] = useState<boolean>(false);
+  const toggleTheme = () => {
+    setIsDark((prev) => !prev);
+  };
+  return (
+    <div className="App">
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <GlobalStyle></GlobalStyle>
+        <Header>
+          <ModeToggleBtn onClick={toggleTheme}>
+            🔁{isDark ? "☀️" : "🌙"}
+          </ModeToggleBtn>
+        </Header>
+        안녕
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  accentColor: "#00a8ff",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  accentColor: "#00a8ff",
+};
